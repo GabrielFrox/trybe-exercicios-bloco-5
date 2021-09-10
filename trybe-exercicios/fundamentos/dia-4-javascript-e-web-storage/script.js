30395c44a20fcfb266c86328abfb46220934604b
Gabriel Oliveira <gabrielfrox@gmail.com>
2021-09-10 00:15:58 -0300

    Fim dos exercicios

--- trybe-exercicios/fundamentos/dia-4-javascript-e-web-storage/script.js
+++ trybe-exercicios/fundamentos/dia-4-javascript-e-web-storage/script.js
@@ -4,6 +4,9 @@ let paragraph = document.querySelector('p')
 let defaultBackColor = ''
 let defaultFontColor = ''
 let defaultFontSize = ''
+let defaultFontHeight = ''
+let defaultFont = ''
+let link = ''
 
 // Função que coloca fundo em modo noturno
 function backNoturno() {
@@ -93,12 +96,74 @@ function revertFontSize() {
 revertFontSize()
 // ---------------------------------
 
+//Função que altera espaçamento do texto
+function fontHeightChange() {
+  let newHeight = document.querySelector('#lineHeight')
+  let heightApplyBtn = document.querySelector('#applyLineHeight')
+  heightApplyBtn.addEventListener('click', function() {
+    heightNum = newHeight.value + 'px';
+    paragraph.style.lineHeight = heightNum;
+    defaultFontHeight = localStorage.setItem('lineHeight', heightNum)
+  })
+}
+fontHeightChange()
+// Auxiliar de reversão
+function revertFontHeight() {
+  let heightRevertBtn = document.querySelector('#revertLineHeight')
+  heightRevertBtn.addEventListener('click', function() {
+    paragraph.style.lineHeight = '18px';
+    defaultFontHeight = localStorage.setItem('lineHeight', '18px')
+  })
+}
+revertFontHeight()
+// ---------------------------------
 
+// Função do primeiro botão das fontes
+function josefinFont() {
+  let josefinF = document.querySelector('#josefin')
+  josefinF.addEventListener('click', function() {
+      paragraph.style.fontFamily = 'Josefin Sans'
+      defaultFont = localStorage.setItem('fontFamily', 'Josefin Sans')
+  });
+}
+josefinFont()
+
+// Função segundo botão fontes
+function permanentFont() {
+  let permanentF = document.querySelector('#marker')
+  permanentF.addEventListener('click', function() {
+      paragraph.style.fontFamily = 'Permanent Marker'
+      defaultFont = localStorage.setItem('fontFamily', 'Permanent Marker')
+  });
+}
+permanentFont()
+
+// Função terceiro botão fontes
+function concertFont() {
+  let concertF = document.querySelector('#concert')
+  concertF.addEventListener('click', function() {
+      paragraph.style.fontFamily = 'Concert One'
+      defaultFont = localStorage.setItem('fontFamily', 'Concert One')
+  });
+}
+concertFont()
+
+// Função que retorna a fonte original
+function resetFont() {
+  let resetStyle = document.querySelector('#defaultBtn');
+  resetStyle.addEventListener('click', function() {
+    paragraph.style.fontFamily = '';
+    defaultFont = localStorage.setItem('fontFamily', '')
+  })
+}
+resetFont()
 // Função que inicia página com localStorage do user
 function newinit() {
   body.style.backgroundColor = localStorage.getItem('backgroundColor')
   body.style.color = localStorage.getItem('fontColor')
   paragraph.style.fontSize = localStorage.getItem('fontSize')
+  paragraph.style.lineHeight = localStorage.getItem('lineHeight')
+  paragraph.style.fontFamily = localStorage.getItem('fontFamily')
 }
 newinit()
 // --------------------------------------------------
